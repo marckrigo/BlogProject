35c9c874855389ef94237eaf7662f4320dd7328b
Renan Garcia <renangarcia@live.com>
2022-02-09 03:34:00 -0300

    feat(Layout): add Layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head"
+import Header from "../Header/Header"
+
+const Layout = ({ children, title}) => {
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <Header/>
+            <main>
+                {children}
+            </main>
+        </>
+    )
+
+}
+
+export default Layout
