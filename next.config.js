7b421f959696f13eec00fda7c5bb358bc4590d51
Renan Garcia <renangarcia@live.com>
2022-02-09 03:31:53 -0300

    chore(): set configuration for SWC

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  swcMinify: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
   env: {
     REACT_AAP_ACCESSKEY: 'KQj9BxHr_XHNTqAea0I7oxX-HcNCeaQ8nxmP9MNHkC8',
   },
